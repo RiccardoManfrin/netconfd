deb4fdfd1ee2b75e44ecb919be28a9e3c1af2564
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-10-14 20:06:13 +0200

    prettify and shut up

--- gen_templates.sh
+++ gen_templates.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 rm server -rf
-
+mkdir server
 #templates/go-server taken from 
 #https://github.com/OpenAPITools/openapi-generator/tree/master/modules/openapi-generator/src/main/resources
-docker run -u $(id -u):$(id -g) --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
+
+docker run \
+    -e GO_POST_PROCESS_FILE="/usr/local/bin/gofmt -w" \
+    -u $(id -u):$(id -g) \
+    --rm \
+    -v "${PWD}:/local" \
+    openapitools/openapi-generator-cli generate \
     -i /local/swaggerui/swagger.yaml -t /local/templates/go-server/ \
-    -g go-server -o /local/server
+    -g go-server -o /local/server 2>&1 > /dev/null
