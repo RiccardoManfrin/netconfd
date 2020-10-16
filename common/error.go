cb6f1824e7504d0a67292883a0e0e10a31d2ada1
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-10-16 11:36:36 +0200

    Fix

--- common/error.go
+++ common/error.go
@@ -9,3 +9,8 @@ type ErrorString struct {
 func (e *ErrorString) Error() string {
 	return e.S
 }
+
+// ToJSONString converts error string to json string
+func ToJSONString(e error) string {
+	return "{\"str\":\"" + e.Error() + "\"}"
+}
