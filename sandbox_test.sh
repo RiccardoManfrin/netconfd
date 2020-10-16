7f5d704a8854bd1ad261cf187f1478cb9b5afc4f
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-10-16 16:08:10 +0200

    Allows for interactive

--- sandbox_test.sh
+++ sandbox_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker network create --driver bridge nc_net
-docker run -v $(pwd):/app --rm -dit --name nc_sandbox --network nc_net --cap-add NET_ADMIN alpine /bin/sh -c "/app/netconfd -log - -config /app/netconfd.json"
+docker run -v $(pwd):/app --rm -it --name nc_sandbox --network nc_net --cap-add NET_ADMIN alpine /bin/sh
+#docker run -v $(pwd):/app --rm -dit --name nc_sandbox --network nc_net --cap-add NET_ADMIN alpine /bin/sh -c "/app/netconfd -log - -config /app/netconfd.json"
 echo "Start testing [press ENTER to unstage test env]"
 read DONE
 docker rm --force nc_sandbox
