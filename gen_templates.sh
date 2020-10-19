009b449c587a15c02435eff2154f269a6f9ededb
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-10-19 14:45:29 +0200

    Use more complete go (client) models instead of go-server ones

--- gen_templates.sh
+++ gen_templates.sh
@@ -23,3 +23,16 @@ docker run \
     openapitools/openapi-generator-cli generate \
     -i /local/swaggerui/swagger.yaml -t /local/templates/go-server/ \
     -g go-server -o /local/server 2>&1 > /dev/null
+
+docker run \
+    -e GO_POST_PROCESS_FILE="/usr/local/bin/gofmt -w" \
+    -u $(id -u):$(id -g) \
+    --rm \
+    -v "${PWD}:/local" \
+    openapitools/openapi-generator-cli generate \
+    -i /local/swaggerui/swagger.yaml \
+    -g go -o /local/server/go-client/ 2>&1 > /dev/null
+
+cp server/go-client/model_*.go server/go/
+cp server/go-client/utils.go server/go/
+rm server/go-client -rf
