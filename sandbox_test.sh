4f4ef86ffb08d6fb19cfad3b9cc7e1a01c0ce0f0
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-10-19 22:50:57 +0200

    Just more duck typing
    for running a shitty container with a shitty network within.
    I need a sandbox env to make it bleed from the eyes

--- sandbox_test.sh
+++ sandbox_test.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-docker network create --driver bridge nc_net
-docker run -v $(pwd):/app --rm -it --name nc_sandbox --network nc_net --cap-add NET_ADMIN alpine /bin/sh
+
+sudo ip netns add nc_net
+sudo ip link add mymacvlan1 link wlan0 type macvlan mode bridge
+sudo ifconfig mymacvlan1 192.168.178.79 up
+sudo ip link set mymacvlan1 netns nc_net
+
+
+sudo podman run -v $(pwd):/app --rm -it --name nc_sandbox --network ns:/var/run/netns/nc_net --cap-add NET_ADMIN alpine /bin/sh
+
+sudo ifconfig mymacvlan1 192.168.178.79 up
+
 #docker run -v $(pwd):/app --rm -dit --name nc_sandbox --network nc_net --cap-add NET_ADMIN alpine /bin/sh -c "/app/netconfd -log - -config /app/netconfd.json"
 echo "Start testing [press ENTER to unstage test env]"
 read DONE
-docker rm --force nc_sandbox
-docker network rm nc_net
+
+#docker rm --force nc_sandbox
+#sudo ip link del mymacvlan1
+sudo ip netns del nc_net
