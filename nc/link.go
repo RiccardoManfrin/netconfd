1080276ceb860d06e5e8e37c3aa08b09c046c1a3
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-11-13 15:52:16 +0100

    Translates HW address

--- nc/link.go
+++ nc/link.go
@@ -12,10 +12,27 @@ type LinkAddrInfo struct {
 	//Broadcast CIDRAddr `json:"broadcast,omitempty"`
 }
 
+// LinkLinkinfoInfoSlaveData Info about slave state/config
+type LinkLinkinfoInfoSlaveData struct {
+	// State of the link:   * `ACTIVE` - Link is actively used   * `BACKUP` - Link is used for failover
+	State string `json:"state,omitempty"`
+	// MII Status:   * `UP`    * `DOWN`
+	MiiStatus string `json:"mii_status,omitempty"`
+	// Number of link failures
+	LinkFailureCount int32 `json:"link_failure_count,omitempty"`
+	// Hardware address
+	PermHwaddr string `json:"perm_hwaddr,omitempty"`
+	// Queue Identifier
+	QueueId int32 `json:"queue_id,omitempty"`
+}
+
 //LinkLinkinfo definition
 type LinkLinkinfo struct {
 	// Type of link layer interface. Supported Types:   * `dummy` - Dummy link type interface for binding intenal services   * `bridge` - Link layer virtual switch type interface   * `bond` - Bond type interface letting two interfaces be seen as one   * `vlan` - Virtual LAN (TAG ID based) interface   * `veth` - Virtual ethernet (with virtual MAC and IP address)   * `macvlan` - Direct virtual eth interface connected to the physical interface,      with owned mac address   * `ipvlan` - Direct virtual eth interface connected to the physical interface.     Physical interface MAC address is reused. L2 type directly connects the lan to      the host phyisical device. L3 type adds a routing layer in between.
 	InfoKind string `json:"info_kind,omitempty"`
+	// FILL ME
+	InfoSlaveKind string                    `json:"info_slave_kind,omitempty"`
+	InfoSlaveData LinkLinkinfoInfoSlaveData `json:"info_slave_data,omitempty"`
 }
 
 //LinkID ifname copy identifier
@@ -29,7 +46,9 @@ type Link struct {
 	// Interface name
 	Ifname string `json:"ifname"`
 	// Maximum Transfer Unit value
-	Mtu      int32          `json:"mtu,omitempty"`
+	Mtu int32 `json:"mtu,omitempty"`
+	// In case the interface is part of a bond or bridge, specifies the bond/bridge interface it belongs to.
+	Master   string         `json:"master,omitempty"`
 	Linkinfo LinkLinkinfo   `json:"linkinfo,omitempty"`
 	LinkType string         `json:"link_type"`
 	Address  string         `json:"address,omitempty"`
@@ -45,6 +64,25 @@ func linkParse(link netlink.Link) Link {
 	nclink.Linkinfo.InfoKind = link.Type()
 	nclink.LinkType = la.EncapType
 	addrs, err := netlink.AddrList(link, netlink.FAMILY_ALL)
+	if la.Slave != nil {
+		nclink.Linkinfo.InfoSlaveKind = la.Slave.SlaveType()
+		switch la.Slave.(type) {
+		case *netlink.BondSlave:
+			{
+				bondslave := la.Slave.(*netlink.BondSlave)
+				nclink.Linkinfo.InfoSlaveData.State = bondslave.State.String()
+				nclink.Linkinfo.InfoSlaveData.MiiStatus = bondslave.MiiStatus.String()
+				nclink.Linkinfo.InfoSlaveData.PermHwaddr = bondslave.PermHardwareAddr.String()
+				nclink.Linkinfo.InfoSlaveData.QueueId = bondslave.QueueId
+				nclink.Linkinfo.InfoSlaveData.LinkFailureCount = bondslave.LinkFailureCount
+			}
+		default:
+			{
+				logger.Log.Warning("Unsupported type of slave/master type interface")
+			}
+		}
+	}
+
 	if err == nil {
 		nclink.AddrInfo = make([]LinkAddrInfo, len(addrs))
 		for i, a := range addrs {
