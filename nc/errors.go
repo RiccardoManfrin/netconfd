b0892b1a931a38b9cf16da832afbcd107a482e4b
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-11-19 10:02:45 +0100

    Bit of restructuring on Errors

--- nc/errors.go
+++ nc/errors.go
@@ -1,78 +1,112 @@
 package nc
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-//ConflictError describes a conflict with the network state and requested changes
-type ConflictError struct {
-	reason string
+type ErrorCode int
+
+const (
+	//CONFLICT error type (inconsistency with respect to the existing state)
+	CONFLICT ErrorCode = iota
+	//SEMANTIC error type of the requested operation in the syntax or logical content
+	SEMANTIC
+	//UNKNOWN_TYPE error type (the value type is not recognized/supported)
+	UNKNOWN_TYPE
+)
+
+var errorCodeToString = map[ErrorCode]string{
+	CONFLICT:     "Conflict Error",
+	SEMANTIC:     "Semantic Error",
+	UNKNOWN_TYPE: "UnknownType Error",
 }
 
+//GenericError describes a generic error of the library
+type GenericError struct {
+	//code error type
+	Code ErrorCode `json:"code"`
+	//reason describes the specific reason for the error
+	Reason string `json:"reason"`
+}
+
+func (e *GenericError) Error() string {
+	strerr, _ := json.Marshal(*e)
+	return string(strerr)
+}
+
+//ConflictError describes a conflict with the network state and requested changes
+type ConflictError GenericError
+
 func (e *ConflictError) Error() string {
-	return fmt.Sprintf("Conflict error: %s", e.reason)
+	strerr, _ := json.Marshal(*e)
+	return string(strerr)
 }
 
 //SemanticError is a logical error on the content of the operation requested to be performed
-type SemanticError ConflictError
+type SemanticError GenericError
 
 func (e *SemanticError) Error() string {
-	return fmt.Sprintf("Semantic error: %s", e.reason)
+	strerr, _ := json.Marshal(*e)
+	return string(strerr)
+}
+
+//UnknownTypeError is a logical error on the content of the operation requested to be performed
+type UnknownTypeError GenericError
+
+func (e *UnknownTypeError) Error() string {
+	strerr, _ := json.Marshal(*e)
+	return string(strerr)
 }
 
 //NewGenericSemanticError returns a generic semantic error
 func NewGenericSemanticError() error {
-	return &SemanticError{reason: "Generic Semantic Error"}
+	return &SemanticError{Code: SEMANTIC, Reason: "Generic Semantic Error"}
 }
 
 //NewBadAddressError returns a bad address error on link layer interfaces
 func NewBadAddressError(c CIDRAddr) error {
-	return &SemanticError{reason: "Bad IP address " + c.String()}
+	return &SemanticError{Reason: "Bad IP address " + c.String()}
 }
 
 //NewLinkExistsConflictError returns a Conflict error on link layer interfaces
 func NewLinkExistsConflictError(linkID LinkID) error {
-	return &ConflictError{reason: "Link " + string(linkID) + " exists"}
+	return &ConflictError{Reason: "Link " + string(linkID) + " exists"}
 }
 
 //NonBondMasterLinkTypeError returns an error for non bond master link type
 func NonBondMasterLinkTypeError(ifname LinkID) error {
-	return &ConflictError{reason: "Master link interface " + string(ifname) + " is not a bond"}
+	return &ConflictError{Reason: "Master link interface " + string(ifname) + " is not a bond"}
 }
 
 //NewLinkNotFoundError returns a Not found error on link layer interfaces
 func NewLinkNotFoundError(linkID LinkID) error {
-	return &NotFoundError{reason: "Link " + string(linkID) + " not found"}
-}
-
-//UnknownTypeError is a logical error on the content of the operation requested to be performed
-type UnknownTypeError SemanticError
-
-func (e *UnknownTypeError) Error() string {
-	return fmt.Sprintf("Unknown type: %s", e.reason)
+	return &NotFoundError{Reason: "Link " + string(linkID) + " not found"}
 }
 
 //NewUnknownLinkKindError returns a UnknownTypeError error on link layer type interfaces
 func NewUnknownLinkKindError(linkKind string) error {
-	return &SemanticError{reason: "LinkKind " + string(linkKind) + " not known"}
+	return &SemanticError{Reason: "LinkKind " + string(linkKind) + " not known"}
 }
 
 //NotFoundError is a logical error on the content of the operation requested to be performed
 type NotFoundError ConflictError
 
 func (e *NotFoundError) Error() string {
-	return fmt.Sprintf("Not Found: %s", e.reason)
+	return fmt.Sprintf("Not Found: %s", e.Reason)
 }
 
 //NewRouteByIDNotFoundError returns a Conflict error on link layer interfaces
 func NewRouteByIDNotFoundError(routeid RouteID) error {
-	return &NotFoundError{reason: "Route ID" + string(routeid) + " did not match"}
+	return &NotFoundError{Reason: "Route ID" + string(routeid) + " did not match"}
 }
 
 //NewActiveSlaveIfaceNotFoundForActiveBackupBondError Returns an error if an active interface is not found for an Active-Backup type bond
 func NewActiveSlaveIfaceNotFoundForActiveBackupBondError(bondIfname LinkID) error {
-	return &SemanticError{reason: "Active Slave Iface not found for Active-Backup type bond " + string(bondIfname)}
+	return &SemanticError{Reason: "Active Slave Iface not found for Active-Backup type bond " + string(bondIfname)}
 }
 
 //NewBackupSlaveIfaceFoundForNonActiveBackupBondError Returns an error if a backup interface is found for a non Active-Backup type bond
 func NewBackupSlaveIfaceFoundForNonActiveBackupBondError(backupIfname LinkID, bondIfname LinkID) error {
-	return &SemanticError{reason: "Backup Slave Iface " + string(backupIfname) + " found for non Active-Backup type bond " + string(bondIfname)}
+	return &SemanticError{Reason: "Backup Slave Iface " + string(backupIfname) + " found for non Active-Backup type bond " + string(bondIfname)}
 }
