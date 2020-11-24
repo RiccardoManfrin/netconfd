16a6f0e3cdab841c65511fe671c243fb55172061
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2020-11-24 22:50:35 +0100

    workout

--- mgr_test.go
+++ mgr_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -199,6 +200,47 @@ func deltaLink(l oas.Link, r oas.Link) string {
 	if lid.GetAdLacpRate() != "" && (lid.GetAdLacpRate() != rid.GetAdLacpRate()) {
 		return "link_info->info_data->ad_lacp_rate"
 	}
+
+	if lid.GetAdLacpRate() != "" && lid.GetAdLacpRate() != rid.GetAdLacpRate() {
+		return fmt.Sprintf("link_info->info_data->AdLacpRate: l:[%v], r:[%v]", lid.GetAdLacpRate(), rid.GetAdLacpRate())
+	}
+	if lid.GetPeerNotifyDelay() != -1 && lid.GetPeerNotifyDelay() != rid.GetPeerNotifyDelay() {
+		return fmt.Sprintf("link_info->info_data->PeerNotifyDelay: l:[%v], r:[%v]", lid.GetPeerNotifyDelay(), rid.GetPeerNotifyDelay())
+	}
+	if lid.GetUseCarrier() != -1 && lid.GetUseCarrier() != rid.GetUseCarrier() {
+		return fmt.Sprintf("link_info->info_data->UseCarrier: l:[%v], r:[%v]", lid.GetUseCarrier(), rid.GetUseCarrier())
+	}
+	if lid.GetLpInterval() != -1 && lid.GetLpInterval() != rid.GetLpInterval() {
+		return fmt.Sprintf("link_info->info_data->LpInterval: l:[%v], r:[%v]", lid.GetLpInterval(), rid.GetLpInterval())
+	}
+	if lid.GetArpAllTargets() != "" && lid.GetArpAllTargets() != rid.GetArpAllTargets() {
+		return fmt.Sprintf("link_info->info_data->ArpAllTargets: l:[%v], r:[%v]", lid.GetArpAllTargets(), rid.GetArpAllTargets())
+	}
+	if lid.GetXmitHashPolicy() != "" && lid.GetXmitHashPolicy() != rid.GetXmitHashPolicy() {
+		return fmt.Sprintf("link_info->info_data->XmitHashPolicy: l:[%v], r:[%v]", lid.GetXmitHashPolicy(), rid.GetXmitHashPolicy())
+	}
+	if lid.GetResendIgmp() != -1 && lid.GetResendIgmp() != rid.GetResendIgmp() {
+		return fmt.Sprintf("link_info->info_data->ResendIgmp: l:[%v], r:[%v]", lid.GetResendIgmp(), rid.GetResendIgmp())
+	}
+	if lid.GetMinLinks() != -1 && lid.GetMinLinks() != rid.GetMinLinks() {
+		return fmt.Sprintf("link_info->info_data->MinLinks: l:[%v], r:[%v]", lid.GetMinLinks(), rid.GetMinLinks())
+	}
+	if lid.GetPrimaryReselect() != "" && lid.GetPrimaryReselect() != rid.GetPrimaryReselect() {
+		return fmt.Sprintf("link_info->info_data->PrimaryReselect: l:[%v], r:[%v]", lid.GetPrimaryReselect(), rid.GetPrimaryReselect())
+	}
+	if lid.GetAdSelect() != "" && lid.GetAdSelect() != rid.GetAdSelect() {
+		return fmt.Sprintf("link_info->info_data->AdSelect: l:[%v], r:[%v]", lid.GetAdSelect(), rid.GetAdSelect())
+	}
+	if lid.GetAllSlavesActive() != -1 && lid.GetAllSlavesActive() != rid.GetAllSlavesActive() {
+		return fmt.Sprintf("link_info->info_data->AllSlavesActive: l:[%v], r:[%v]", lid.GetAllSlavesActive(), rid.GetAllSlavesActive())
+	}
+
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpInterval(500)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpValidate("backup")
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPacketsPerSlave(2)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetFailOverMac()
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetTlbDynamicLb(1)
+
 	return ""
 }
 
@@ -242,6 +284,55 @@ func Test006(t *testing.T) {
 	cset := genSampleConfig()
 	*(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.Mode = "802.3ad"
 	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAdLacpRate("fast")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPeerNotifyDelay(2000)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetUseCarrier(0)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpInterval(500)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpValidate("backup")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetLpInterval(2)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpAllTargets("all")
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPacketsPerSlave(2)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetFailOverMac()
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetXmitHashPolicy("layer2+3")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetResendIgmp(3)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetMinLinks(2)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPrimaryReselect("better")
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetTlbDynamicLb(1)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAdSelect("bandwidth")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAllSlavesActive(1)
+	(*cset.HostNetwork.Links)[1].Linkinfo.InfoSlaveData.SetState("ACTIVE")
+	rr := runConfigSet(cset)
+	checkResponse(t, rr, http.StatusOK, nc.RESERVED, "")
+	cget := runConfigGet()
+	cLinksSetMap := listToMap(*cset.HostNetwork.Links, "Ifname")
+	cLinksGetMap := listToMap(*cget.HostNetwork.Links, "Ifname")
+	for ifname, setLink := range cLinksSetMap {
+		getLink := cLinksGetMap[ifname].(oas.Link)
+		if delta := deltaLink(setLink.(oas.Link), getLink); delta != "" {
+			t.Errorf("Mismatch on %v", delta)
+		}
+	}
+}
+
+//Test007 - OK-007 Bond Balance-RR Xmit Hash Policy params check
+func Test007(t *testing.T) {
+	cset := genSampleConfig()
+	*(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.Mode = "802.3ad"
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAdLacpRate("fast")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPeerNotifyDelay(2000)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetUseCarrier(0)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpInterval(500)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpValidate("backup")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetLpInterval(2)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetArpAllTargets("all")
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPacketsPerSlave(2)
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetFailOverMac()
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetXmitHashPolicy("layer2+3")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetResendIgmp(3)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetMinLinks(2)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetPrimaryReselect("better")
+	//(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetTlbDynamicLb(1)
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAdSelect("bandwidth")
+	(*cset.HostNetwork.Links)[0].Linkinfo.InfoData.SetAllSlavesActive(1)
 	(*cset.HostNetwork.Links)[1].Linkinfo.InfoSlaveData.SetState("ACTIVE")
 	rr := runConfigSet(cset)
 	checkResponse(t, rr, http.StatusOK, nc.RESERVED, "")
