0a071fa3ff8e001f40510d92758c999499bb18d2
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2021-01-28 23:04:07 +0100

    Rip off dns set/get for now .. you need to use this
    root@ngcore:~# resolvectl dns eth0 192.168.178.1  8.8.8.8
    root@ngcore:~# resolvectl dns eth0
    Link 2 (eth0): 192.168.178.1 8.8.8.8

--- nc/dns.go
+++ nc/dns.go
@@ -1,16 +1,7 @@
 package nc
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"net"
-	"os"
-	"regexp"
-	"strings"
-
-	"gitlab.lan.athonet.com/core/netconfd/logger"
 )
 
 type DnsID string
@@ -42,97 +33,12 @@ const ResolvConf string = "/etc/resolv.conf"
 
 func loadResolv() []Dns {
 	dnss := make([]Dns, 0)
-	file, err := os.Open(ResolvConf)
-	if err != nil {
-		logger.Fatal(err)
-		return dnss
-	}
-	defer file.Close()
 
-	// Start reading from the file with a reader.
-	reader := bufio.NewReader(file)
-
-	prio := 1
-	/*
-	* https://man7.org/linux/man-pages/man5/resolv.conf.5.html
-	*
-	* The keyword and value must appear on a single line, and the
-	* keyword (e.g., nameserver) must start the line.  The value
-	* follows the keyword, separated by white space.
-	 */
-	re := regexp.MustCompile(`^nameserver {1}[^\s].*`)
-
-	for {
-		line, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-
-		if re.MatchString(line) {
-			ns := strings.Replace(line, "nameserver", "", 1)
-			ns = strings.ReplaceAll(ns, " ", "")
-			ns = strings.ReplaceAll(ns, "\n", "")
-			dns := Dns{
-				Nameserver: net.ParseIP(ns),
-				Id:         dnsIDToString[prio],
-			}
-			prio++
-			dnss = append(dnss, dns)
-			if prio > 2 {
-				break
-			}
-		}
-	}
 	return dnss
 }
 func dumpResolv(dnss []Dns) error {
-	if len(dnss) > 2 {
-		return NewTooManyDNSServersError()
-	}
-	resolvConf := ""
-	var pri *Dns = nil
-	var sec *Dns = nil
 
-	if len(dnss) > 0 {
-		if dnss[0].Id == DnsPrimary {
-			pri = &dnss[0]
-		} else if dnss[0].Id == DnsSecondary {
-			sec = &dnss[0]
-		} else {
-			return NewUnknownUnsupportedDNSServersIDsError(dnss[0].Id)
-		}
-
-		if len(dnss) == 2 {
-			if dnss[1].Id == DnsPrimary {
-				if pri != nil {
-					return NewDuplicateDNSServersIDsError(dnss[0].Id, dnss[1].Id)
-				}
-				pri = &dnss[1]
-			} else if dnss[1].Id == DnsSecondary {
-				if sec != nil {
-					return NewDuplicateDNSServersIDsError(dnss[0].Id, dnss[1].Id)
-				}
-				sec = &dnss[1]
-			} else {
-				return NewUnknownUnsupportedDNSServersIDsError(dnss[1].Id)
-			}
-		}
-		if pri != nil {
-			resolvConf = fmt.Sprintf(
-				"%vnameserver %v\n",
-				resolvConf,
-				pri.Nameserver.String())
-		}
-		if sec != nil {
-			resolvConf = fmt.Sprintf(
-				"%vnameserver %v\n",
-				resolvConf,
-				sec.Nameserver.String())
-		}
-	}
-	return ioutil.WriteFile(
-		ResolvConf,
-		[]byte(resolvConf), 777)
+	return nil
 }
 
 func DNSCreate(dns Dns) error {
