50b6ec261931d737a7e4722f17f8ac663ed10aa0
Riccardo Manfrin <Riccardo.Manfrin@athonet.com>
2021-03-31 17:16:47 +0200

    Adds openapi validation errors

--- server/go/errors.go
+++ server/go/errors.go
@@ -1,11 +1,24 @@
 package openapi
 
 import (
+	"fmt"
+	"runtime/debug"
+
 	"gitlab.lan.athonet.com/core/netconfd/nc"
 )
 
-//NewGenericSemanticError returns a generic semantic error
-func NewGenericSemanticError() error {
+//NewAttributeDoesntBelongToLinkKindSemanticError returns an error for an attribute not belonging to the discriminated object
+func NewAttributeDoesntBelongToLinkKindSemanticError(attribKey string, infoKind string) error {
+	if nc.NetconfdDebugTrace {
+		debug.PrintStack()
+	}
+	return &nc.SemanticError{Code: nc.SEMANTIC, Reason: fmt.Sprintf("Attribute %s doesn't belong to Link Kind %s", attribKey, infoKind)}
+}
 
-	return &nc.SemanticError{Code: nc.SEMANTIC, Reason: "Generic Semantic Error"}
+//NewMissingRequiredAttributeForLinkKindSemanticError returns an error for a missing attribute
+func NewMissingRequiredAttributeForLinkKindSemanticError(attribKey string, infoKind string) error {
+	if nc.NetconfdDebugTrace {
+		debug.PrintStack()
+	}
+	return &nc.SemanticError{Code: nc.SEMANTIC, Reason: fmt.Sprintf("Missing required attribute %s for Link Kind %s", attribKey, infoKind)}
 }
